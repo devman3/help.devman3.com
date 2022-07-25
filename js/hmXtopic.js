8e06a21076cd92e5124a646043ce815ae461641c
Duncan Kaunda <duncankaunda@gmail.com>
2022-07-25 23:21:21 +0200

    Publish
    22/07/25

--- js/hmXtopic.js
+++ js/hmXtopic.js
@@ -6,7 +6,7 @@ hmProjectInfo = {
 	copyright: "© 2022 IS Kidz Africa Pty Ltd",
 	summary: "Proud authors or Devman Bursary, Fundraising and Grant-making &nbsp;web application",
 	version: "4.11.20160229",
-	date: "Sunday, 17 July 2022",
+	date: "Monday, 25 July 2022",
 	language: "en-za",
 	mainfile: "index.html"
 	}
