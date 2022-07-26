6849136598a82088db72d24cdf1d0b9ddb6fab36
Duncan Kaunda <duncankaunda@gmail.com>
2022-07-26 14:56:33 +0200

    Publish
    22/07/26 Redo

---
+++ jspopups/gsypopi.js
@@ -0,0 +1,4 @@
+hmLoadPopup({
+hmTitle:"gsyPOPI",
+hmBody:"<p class=\"p_KidzNormal\"><span class=\"f_KidzNormal\">POPI - Protection of personal information Act - No 4 of 2013 promotes the protection of personal information by public and private bodies.<\/span><\/p>\n\r"
+})
---
+++ jstopics/actinteraction.js
@@ -0,0 +1,12 @@
+hmLoadTopic({
+hmKeywords:"",
+hmTitle:"Interactions",
+hmDescription:"As interactions are recorded on the system, you are notified in the Weekly activity section of the Home page:",
+hmPrevLink:"",
+hmNextLink:"",
+hmParentLink:"devmanfunction.html",
+hmBreadCrumbs:"",
+hmTitlePath:"DevMan functions > Workflow",
+hmHeader:"<p class=\"p_KidzHeader1\"><span class=\"f_KidzHeader1\">Interactions<\/span><\/p>\n\r",
+hmBody:"<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">As <a href=\"javascript:void(0);\" data-target=\"gsyinteraction.js\" class=\"popuplink\">interactions<\/a> are recorded on the system, you are notified in the <a href=\"javascript:void(0);\" data-target=\"gsyhomepageinteraction.js\" class=\"popuplink\">Weekly activity section<\/a> of the Home page:<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">You may view these new interactions by clicking on this notification.<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">To &nbsp;see older interactions goto <a href=\"actinteractionview.html\" class=\"topiclink\">View a past interaction<\/a> <\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">You can go add an interaction directly via communications or from a contact. See how to <a href=\"actinteractionadd.html\" class=\"topiclink\">add an interaction<\/a>.<\/span><\/p>\n\r"
+})
