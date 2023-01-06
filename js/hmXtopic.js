d91bf350260aaeb18aecc3cbeba4cb1e1700971f
Aleczander Mehl <aleczander@devman3.com>
2023-01-06 15:06:30 +0200

    2023/01/06
    •	Drop Donation deposit process guide link
    •	Move Bursary process guide to Bursary Management
    •	Complete Bank deposits edit

--- js/hmXtopic.js
+++ js/hmXtopic.js
@@ -3,10 +3,10 @@
 hmProjectInfo = {
 	title: "DevMan Help",
 	author: "IS Kidz Africa Pty Ltd",
-	copyright: "© 2022 IS Kidz Africa Pty Ltd",
+	copyright: "© 2023 IS Kidz Africa Pty Ltd",
 	summary: "Proud authors or Devman Bursary, Fundraising and Grant-making &nbsp;web application",
 	version: "4.11.20160229",
-	date: "Friday, 09 December 2022",
+	date: "Friday, 06 January 2023",
 	language: "en-za",
 	mainfile: "index.html"
 	}
