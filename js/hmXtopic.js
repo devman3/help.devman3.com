648f930acd167513cce8ef2db6755a77ffc3a147
Aleczander Mehl <aleczander@devman3.com>
2023-04-03 14:58:34 +0200

    2023/04/03
    Topics covered:
    
    •	Adjustments in features of tax certificates
    •	Adjustments in new field requirements
    •	New blurb and new Xplain for sample template
    •	Self-service link - added blurb into email or phone updates.
    •	Xplain - added SARS type, then SARS detail update for manual update in second half.

--- js/hmXtopic.js
+++ js/hmXtopic.js
@@ -6,7 +6,7 @@ hmProjectInfo = {
 	copyright: "© 2023 IS Kidz Africa Pty Ltd",
 	summary: "Proud authors or Devman Bursary, Fundraising and Grant-making &nbsp;web application",
 	version: "4.11.20160229",
-	date: "Friday, 24 March 2023",
+	date: "Monday, 03 April 2023",
 	language: "en-za",
 	mainfile: "index.html"
 	}
