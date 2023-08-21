bfd1adafa9f6eb8016659a3c95cc0a32c6f436ba
Carolann9 <carol@devman3.com>
2023-08-21 14:31:55 +0200

    2023/08/21 14:31

--- js/hmXtopic.js
+++ js/hmXtopic.js
@@ -6,7 +6,7 @@ hmProjectInfo = {
 	copyright: "© 2023 IS Kidz Africa Pty Ltd",
 	summary: "Proud authors or Devman Bursary, Fundraising and Grant-making &nbsp;web application",
 	version: "4.11.20160229",
-	date: "Thursday, 17 August 2023",
+	date: "Monday, 21 August 2023",
 	language: "en-za",
 	mainfile: "index.html"
 	}
