edd60e33b0bf9dd1f2c8db7097ce57d481c8934f
Devman3 <111979134+devman3@users.noreply.github.com>
2024-07-11 14:06:31 +0200

    Manual upload
    Repopulate GIT

---
+++ jstopics/navigation2.js
@@ -0,0 +1,12 @@
+hmLoadTopic({
+hmKeywords:"",
+hmTitle:"Navigation",
+hmDescription:"Topics  How to manage bookmarks and favorites, and navigate back to previous page.  Navigation",
+hmPrevLink:"",
+hmNextLink:"",
+hmParentLink:"devmanfunction.html",
+hmBreadCrumbs:"",
+hmTitlePath:"DevMan functions > Workflow",
+hmHeader:"<p class=\"p_KidzHeader1\"><span class=\"f_KidzHeader1\">Navigation<\/span><\/p>\n\r",
+hmBody:"<p class=\"p_KidzHeader12\"><span class=\"f_KidzHeader12\">Topics<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">How to manage bookmarks and favorites, and navigate back to previous page.<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\" style=\"text-indent: 0; padding-left: 0.8125rem; margin-left: 2.3750rem;\"><span class=\"f_KidzNormalIndent\" style=\"font-family: Arial,\'Lucida Sans Unicode\',\'Lucida Grande\',\'Lucida Sans\';display:inline-block;width:0.8125rem;margin-left:-0.8125rem\">&#8226;<\/span><span class=\"f_KidzNormalIndent\"><a href=\"navigation.html#faqnavigation\" class=\"topiclink\" title=\"Navigation\">Navigation<\/a><\/span><\/p>"
+})
