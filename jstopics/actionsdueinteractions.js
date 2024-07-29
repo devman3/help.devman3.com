ab84c658695f2b618595ab12b95d681cd2e55c84
Carol Ann Sauls <carol@devman3.com>
2024-07-29 14:35:06 +0200

    2024/07/29 14:35

---
+++ jstopics/actionsdueinteractions.js
@@ -0,0 +1,12 @@
+hmLoadTopic({
+hmKeywords:"",
+hmTitle:"Actions due\/Interactions",
+hmDescription:"Overview    Topics  Actions due Activities \/ Interactions",
+hmPrevLink:"mailshot.html",
+hmNextLink:"actionsdue.html",
+hmParentLink:"devcommunication.html",
+hmBreadCrumbs:"<a href=\"devcommunication.html\">Communication<\/a>",
+hmTitlePath:"Communication > Actions due\/Interactions",
+hmHeader:"<p class=\"p_KidzHeader1New\"><span class=\"f_KidzHeader1New\">Actions due\/Interactions<\/span><\/p>\n\r",
+hmBody:"<p class=\"p_KidzHeader12New\"><span class=\"f_KidzHeader12New\">Overview<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r<p class=\"p_KidzHeader12New\"><span class=\"f_KidzHeader12New\">Topics<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\" style=\"text-indent: 0; padding-left: 0.8125rem; margin-left: 2.3750rem;\"><span class=\"f_KidzNormalIndent\" style=\"font-family: Arial,\'Lucida Sans Unicode\',\'Lucida Grande\',\'Lucida Sans\';display:inline-block;width:0.8125rem;margin-left:-0.8125rem\">&#8226;<\/span><span class=\"f_KidzNormalIndent\"><a href=\"actionsdue.html\" class=\"topiclink\" title=\"Actions due\">Actions due<\/a><\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\" style=\"text-indent: 0; padding-left: 0.8125rem; margin-left: 2.3750rem;\"><span class=\"f_KidzNormalIndent\" style=\"font-family: Arial,\'Lucida Sans Unicode\',\'Lucida Grande\',\'Lucida Sans\';display:inline-block;width:0.8125rem;margin-left:-0.8125rem\">&#8226;<\/span><span class=\"f_KidzNormalIndent\"><a href=\"activitiesinteraction.html\" class=\"topiclink\" title=\"Activities Interactions\">Activities \/ Interactions<\/a><\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r"
+})
---
+++ jstopics/remittances.js
@@ -0,0 +1,12 @@
+hmLoadTopic({
+hmKeywords:"",
+hmTitle:"Remittances",
+hmDescription:"Enter topic text here.",
+hmPrevLink:"payments.html",
+hmNextLink:"importsandupdates.html",
+hmParentLink:"expenditure.html",
+hmBreadCrumbs:"<a href=\"devmanfunction.html\">DevMan functions<\/a> &gt; <a href=\"devfinance.html\">Finance<\/a> &gt; <a href=\"expenditure.html\">Expenditure<\/a>",
+hmTitlePath:"DevMan functions > Finance > Expenditure > Remittances",
+hmHeader:"<p class=\"p_KidzHeader1New\"><span class=\"f_KidzHeader1New\">Remittances<\/span><\/p>\n\r",
+hmBody:"<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">Enter topic text here.<\/span><\/p>\n\r"
+})
