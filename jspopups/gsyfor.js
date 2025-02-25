fe0abeba627c52f981368d6940680adc24499350
Carol Ann Sauls <carol@devman3.com>
2025-02-25 09:08:16 +0200

    2025/02/25 09:08

--- jspopups/gsyfor.js
+++ jspopups/gsyfor.js
@@ -1,4 +1,4 @@
 hmLoadPopup({
 hmTitle:"For",
-hmBody:"<p class=\"p_KidzNormal\"><span class=\"f_KidzNormal\">You interact with a person <\/span><span class=\"f_KidzNormal\" style=\"font-weight: bold;\">for<\/span><span class=\"f_KidzNormal\"> a project.<\/span><\/p>\n\r<p class=\"p_KidzNormal\"><span class=\"f_KidzNormal\">&nbsp;<\/span><\/p>\n\r<p class=\"p_KidzNormal\"><span class=\"f_KidzNormal\">&nbsp;<\/span><\/p>\n\r"
+hmBody:"<p class=\"p_KidzNormal\"><span class=\"f_KidzNormal\">You interact with a person <\/span><strong class=\"f_KidzNormal\" style=\"font-weight: bold;\">for<\/strong><span class=\"f_KidzNormal\"> a project.<\/span><\/p>\n\r<p class=\"p_KidzNormal\"><span class=\"f_KidzNormal\">&nbsp;<\/span><\/p>\n\r<p class=\"p_KidzNormal\"><span class=\"f_KidzNormal\">&nbsp;<\/span><\/p>\n\r"
 })
