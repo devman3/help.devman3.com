2e05fd9c6f922959a43180357acfab18124617a8
Carol Ann Sauls <carol@devman3.com>
2025-11-11 15:15:47 +0200

    2025/11/11 15:15

---
+++ jstopics/flags.js
@@ -0,0 +1,12 @@
+hmLoadTopic({
+hmKeywords:"",
+hmTitle:"Flags",
+hmDescription:"Overview    Topics",
+hmPrevLink:"payments.html",
+hmNextLink:"importsandupdates.html",
+hmParentLink:"devmanfunction.html",
+hmBreadCrumbs:"<a href=\"devmanfunction.html\">DevMan functions<\/a>",
+hmTitlePath:"DevMan functions > Flags",
+hmHeader:"<p class=\"p_KidzHeader1New\"><span class=\"f_KidzHeader1New\">Flags<\/span><\/p>\n\r",
+hmBody:"<p class=\"p_KidzHeader12New\"><span class=\"f_KidzHeader12New\">Overview<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r<p class=\"p_KidzHeader12\"><span class=\"f_KidzHeader12\">Topics<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r"
+})
