1c177ef011327384449cac672d983c5a9655c89f
Carol Ann Sauls <carol@devman3.com>
2025-11-14 13:57:12 +0200

    2025/11/14 13:57

--- jstopics/knownissues.js
+++ jstopics/knownissues.js
@@ -1,11 +1,12 @@
 hmLoadTopic({
 hmKeywords:"",
 hmTitle:"Known issues",
+hmDescription:"Issues  There are currently no known issues on DevMan.",
 hmPrevLink:"faqsupport.html",
 hmNextLink:"whatsnew.html",
 hmParentLink:"index.html",
 hmBreadCrumbs:"<a href=\"index.html\">Introduction<\/a>",
 hmTitlePath:"Introduction > Known issues",
 hmHeader:"<p class=\"p_KidzHeader1New\"><span class=\"f_KidzHeader1New\">Known issues<\/span><\/p>\n\r",
-hmBody:"<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r"
+hmBody:"<p class=\"p_KidzHeader12New\"><span class=\"f_KidzHeader12New\">Issues<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">There are currently no known issues on DevMan.<\/span><\/p>\n\r<p class=\"p_KidzNormalIndent\"><span class=\"f_KidzNormalIndent\">&nbsp;<\/span><\/p>\n\r"
 })
